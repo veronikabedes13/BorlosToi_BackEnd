31190be4997718d04c71d0a45adda61062fef083
veronikabedes13 <veronikabedes66029@gmail.com>
2024-09-25 14:04:40 +0800

    first init

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.get('/',(req, res) => {
+    res.json({
+        status: "success",
+        message: "get succeed"
+    })
+})
+
+
+app.listen(port, () => 
+    console.log(`Server running at http://localhost:${port}`)
+)
